8044be5bdced50fcf5e2fee1830c7e01bb80cd51
checkiamsiam <issiam02415@gmail.com>
2025-03-08 19:47:12 +0600

    feat: Implement Latest Updates tab with comprehensive card components

--- src/pages/home/main-tabs/_components/latest-updates/index.tsx
+++ src/pages/home/main-tabs/_components/latest-updates/index.tsx
@@ -1,5 +1,11 @@
 import { Box, Group, SimpleGrid, Text } from "@mantine/core";
 import CustomizeButton from "./_components/customize-button";
+import FavoriteCard from "./_components/favorite-card";
+import LatestDocumentsCard from "./_components/latest-document-card";
+import LatestMandatesCard from "./_components/latest-mandates";
+import LatestNewsCard from "./_components/latest-news-card";
+import PlanReturnsCard from "./_components/plan-returns-card";
+import TasksCard from "./_components/task-card";
 
 const LatestUpdatesTab = () => {
   return (
@@ -16,7 +22,176 @@ const LatestUpdatesTab = () => {
         spacing={{ base: 10, sm: "md" }}
         verticalSpacing={{ base: "md", sm: "md" }}
       >
-        {/* TODO: Add latest updates */}
+        <LatestDocumentsCard
+          documents={[
+            {
+              title: "California Public Employee's Retirement System",
+              filename: "Tech requirements.pdf",
+              href: "#",
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              filename: "Tech requirements.pdf",
+              href: "#",
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              filename: "Tech requirements.pdf",
+              href: "#",
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              filename: "Tech requirements.pdf",
+              href: "#",
+            },
+          ]}
+        />
+        <LatestMandatesCard
+          documents={[
+            {
+              title: "California Public Employee's Retirement System",
+              filename: "Tech requirements.pdf",
+              href: "#",
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              filename: "Tech requirements.pdf",
+              href: "#",
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              filename: "Tech requirements.pdf",
+              href: "#",
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              filename: "Tech requirements.pdf",
+              href: "#",
+            },
+          ]}
+        />
+        <PlanReturnsCard
+          documents={[
+            {
+              title: "California Public Employee's Retirement System",
+              href: "#",
+              performanceItems: [
+                { label: "QTD", value: "-1.97%" },
+                { label: "QTD", value: "-1.97%" },
+                { label: "QTD", value: "-1.97%" },
+              ],
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              href: "#",
+              performanceItems: [
+                { label: "QTD", value: "-1.97%" },
+                { label: "QTD", value: "-1.97%" },
+                { label: "QTD", value: "-1.97%" },
+              ],
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              href: "#",
+              performanceItems: [
+                { label: "QTD", value: "-1.97%" },
+                { label: "QTD", value: "-1.97%" },
+                { label: "QTD", value: "-1.97%" },
+              ],
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              href: "#",
+              performanceItems: [
+                { label: "QTD", value: "-1.97%" },
+                { label: "QTD", value: "-1.97%" },
+                { label: "QTD", value: "-1.97%" },
+              ],
+            },
+          ]}
+        />
+        <LatestNewsCard
+          newsItems={[
+            {
+              title: "News Heading",
+              date: "Feb 5, 2025",
+              content:
+                "Id sed pharetra morbi in tempus dignissim imperdiet metus eget. Risus semper senectus orci neque nulla netus ultrices est ut. Varius semper.",
+              href: "#",
+              type: "ria",
+            },
+            {
+              title: "News Heading",
+              date: "Feb 5, 2025",
+              content:
+                "Id sed pharetra morbi in tempus dignissim imperdiet metus eget. Risus semper senectus orci neque nulla netus ultrices est ut. Varius semper.",
+              href: "#",
+              type: "consultants",
+            },
+            {
+              title: "News Heading",
+              date: "Feb 5, 2025",
+              content:
+                "Id sed pharetra morbi in tempus dignissim imperdiet metus eget. Risus semper senectus orci neque nulla netus ultrices est ut. Varius semper.",
+              href: "#",
+              type: "managers",
+            },
+            {
+              title: "News Heading",
+              date: "Feb 5, 2025",
+              content:
+                "Id sed pharetra morbi in tempus dignissim imperdiet metus eget. Risus semper senectus orci neque nulla netus ultrices est ut. Varius semper.",
+              href: "#",
+              type: "managers",
+            },
+          ]}
+        />
+        <TasksCard
+          tasks={[
+            {
+              title: "California Public Employee's Retirement System",
+              description: "Portfolio Manager Goverment CHaritable",
+              href: "#",
+              type: "today",
+            },
+            {
+              title: "Ponte Corgo Government Charitable Nonprofit Foundation",
+              description: "Portfolio Manager Goverment CHaritable",
+              href: "#",
+              type: "today",
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              description: "Portfolio Manager Goverment CHaritable",
+              href: "#",
+              type: "thisMonth",
+            },
+          ]}
+        />
+        <FavoriteCard
+          items={[
+            {
+              title: "California Public Employee's Retirement System",
+              href: "#",
+              type: "consultants",
+            },
+            {
+              title: "Ponte Corgo Government Charitable Nonprofit Foundation",
+              href: "#",
+              type: "managers",
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              href: "#",
+              type: "ria",
+            },
+            {
+              title: "California Public Employee's Retirement System",
+              href: "#",
+              type: "plans",
+            },
+          ]}
+        />
       </SimpleGrid>
     </Box>
   );
