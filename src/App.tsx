ba98433ea375b39b793d92b7c486b7519fa8c3d8
checkiamsiam <issiam02415@gmail.com>
2025-03-08 18:00:11 +0600

    feat: Add PopoverProviders wrapper to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { emotionTransform, MantineEmotionProvider } from "@mantine/emotion";
 import { RouterProvider } from "react-router";
+import PopoverProviders from "./components/providers/popover-providers";
 import router from "./routes";
 
 export default function App() {
   return (
     <MantineProvider stylesTransform={emotionTransform}>
       <MantineEmotionProvider>
-        <RouterProvider router={router}  />
+        <PopoverProviders>
+          <RouterProvider router={router} />
+        </PopoverProviders>
       </MantineEmotionProvider>
     </MantineProvider>
   );
